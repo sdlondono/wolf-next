9c05ed4dc8fb8fec144aff06f990c74dab40e3da
ConDish <samioff15@gmail.com>
2019-04-30 15:35:41 -0500

    Header modifiqued

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const Header = () => (
     <nav className="flex items-center justify-between flex-wrap bg-primary-wolf p-6">
         {/** ============================= Logo of the company ============================= */}
         <div className="flex items-center flex-no-shrink text-white mr-6">
-            <svg className="fill-current h-5 w-10 pr-4 justify-start items-start" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+            <svg className="fill-current h-5 w-10 pr-4 justify-start items-start hover:text-indigo-darker cursor-pointer" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
             <span className="font-semibold text-xl tracking-tight">Wolf</span>
         </div>
 
